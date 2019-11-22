fa3d13ff7ab514225df3bcf9879641a02b5129fa
chrvaz <christian.vazques@knowit.se>
2019-11-22 09:39:13 +0100

    initial commit

---
+++ src/main/java/se/knowit/bookitnotification/BookitNotificationApplication.java
@@ -0,0 +1,13 @@
+package se.knowit.bookitnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookitNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookitNotificationApplication.class, args);
+	}
+
+}
